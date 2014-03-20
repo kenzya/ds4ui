f484aef40b73293f4cb39b1a0d5109422b8e2923
donbabbeo <donbabbeo@gmail.com>
2014-03-20 17:16:25 +0100

    Added comments on ControllerConfigurationLibrary

--- ControllerConfigurationLibrary/ControllerConfigurationManager.cs
+++ ControllerConfigurationLibrary/ControllerConfigurationManager.cs
@@ -3,37 +3,51 @@ using System.IO;
 
 namespace ControllerConfigurationLibrary
 {
+    /// <summary>
+    /// Library used to read and write controller's configuration data
+    /// </summary>
+    /// TODO: considering the similarities a merge with ConfigurationLibrary
+    /// it's a good idea
     public class ControllerConfigurationManager : IControllerConfigurationManager
     {
         #region fields
 
+        // Path of the controller's directory
         private readonly string mainPath;
+
+        // Dictionary of the providers (one for each controller)
+        // TODO: replace string with int when we switch to an ID-based system for the controllers
         private readonly Dictionary<string, XmlConfigProvider> dictionary;
 
         #endregion
 
         #region constructor
 
+        /// <summary>
+        /// Constructor. Set the path of the directory and create a provider for each 
+        /// configuration file.
+        /// </summary>
+        /// <param name="path"></param>
         public ControllerConfigurationManager(string path)
         {
             mainPath = path;
             dictionary = new Dictionary<string, XmlConfigProvider>();
-            Initialize(mainPath);
+
+            foreach (string file in Directory.GetFiles(mainPath, "*.xml"))
+            {
+                XmlConfigProvider provider = new XmlConfigProvider(file);
+                dictionary.Add(Path.GetFileNameWithoutExtension(file), provider);
+            }
         }
         
         #endregion
 
         #region methods
 
-        public void Initialize(string path)
-        {
-            foreach (string file in Directory.GetFiles(path, "*.xml"))
-            {
-                XmlConfigProvider provider = new XmlConfigProvider(file);
-                dictionary.Add(Path.GetFileNameWithoutExtension(file), provider);
-            }
-        }
-
+        /// <summary>
+        /// Set the value of a controller's configuration property
+        /// </summary>
+        /// TODO: switch to int instead of string for key dictionary
         public void SetData(string key, string name, string value)
         { 
             if (!dictionary.ContainsKey(name))
@@ -44,6 +58,10 @@ namespace ControllerConfigurationLibrary
             dictionary[name].SetValue(key, value);
         }
 
+        /// <summary>
+        /// Get the value of a controller's configuration property
+        /// </summary>
+        /// TODO: switch to int instead of string for key dictionary
         public string GetData(string key, string name)
         {
             if (!dictionary.ContainsKey(name))
@@ -54,6 +72,10 @@ namespace ControllerConfigurationLibrary
             return dictionary[name].GetValue(key);
         }
 
+        /// <summary>
+        /// Get the default value of a controller's configuration property
+        /// </summary>
+        /// TODO: switch to int instead of string for key dictionary
         public string GetDefault(string key, string name)
         {
             if (!dictionary.ContainsKey(name))
@@ -64,6 +86,11 @@ namespace ControllerConfigurationLibrary
             return dictionary[name].GetDefault(key);
         }
 
+        /// <summary>
+        /// Create a new configuration file using the default configuration as a model.
+        /// When we get or set a value and the corrisponding provider is not found, this
+        /// means that it's a new controller and thus a new configuration should be created.
+        /// </summary>
         private void CreateNewConfiguration(string directory, string name)
         {
             string defaultFile = Path.Combine(directory, "default_controller.xml");
--- ControllerConfigurationLibrary/IControllerConfigurationManager.cs
+++ ControllerConfigurationLibrary/IControllerConfigurationManager.cs
@@ -1,14 +1,13 @@
 ﻿
 namespace ControllerConfigurationLibrary
 {
+    /// <summary>
+    /// Interface implemented by the configuration
+    /// </summary>
     public interface IControllerConfigurationManager
     {
-        void Initialize(string path);
-        
-        void SetData(string key, string name, string value);
-        
+        void SetData(string key, string name, string value);      
         string GetData(string key, string name);
-       
         string GetDefault(string key, string name);
     }
 }
